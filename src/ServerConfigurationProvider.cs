307025d69bbbae3fd93dd75e945574cef4b4044e
CodeBeast357 <270547+CodeBeast357@users.noreply.github.com>
2019-04-03 15:39:21 -0400

    Updated ServerConfigurationProvider to only take account of new or updated settings as flags

--- src/ServerConfigurationProvider.cs
+++ src/ServerConfigurationProvider.cs
@@ -21,27 +21,27 @@ namespace JotunShard.Configuration
 
         public override void Load()
         {
-            var newSettings = new HashSet<string>(_keyComparer);
+            var hasNewSettings = false;
             var oldSettings = new HashSet<string>(Data.Keys, _keyComparer);
-            var changedSettings = new HashSet<string>(_keyComparer);
+            var hasChangedSettings = false;
             var serverConfiguration = _server.QuerySettings();
             if (serverConfiguration == null) return;
             foreach (var setting in serverConfiguration)
             {
                 if (!TryGet(setting.Key, out var oldValue))
-                    newSettings.Add(setting.Key);
+                    hasNewSettings = true;
                 else
                 {
                     oldSettings.Remove(setting.Key);
                     if (oldValue != setting.Value)
-                        changedSettings.Add(setting.Key);
+                        hasChangedSettings = true;
                 }
                 Set(setting.Key, setting.Value);
             }
             foreach (var setting in oldSettings) Data.Remove(setting);
-            if (newSettings.Count != 0
+            if (hasNewSettings
                 || oldSettings.Count != 0
-                || changedSettings.Count != 0)
+                || hasChangedSettings)
                 OnReload();
         }
     }
