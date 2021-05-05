27150e62ed678f6147f379956996845c78bb0aaf
CodeBeast357 <270547+CodeBeast357@users.noreply.github.com>
2021-05-05 19:41:30 -0400

    Added Vault Configuration

---
+++ Vault/VaultServerConfigurationSource.cs
@@ -0,0 +1,10 @@
+﻿namespace JotunShard.Configuration.Vault
+{
+    public class VaultServerConfigurationSource : ServerConfigurationSource
+    {
+        public override IConfigurationServerProvider ServerProvider { get; }
+            = new VaultServerConfigurationServerProvider();
+
+        public VaultServerConfigurationSource() { }
+    }
+}
