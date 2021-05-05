d37b4ba5a9983e82de138aeb1b5b9797cc3de401
CodeBeast357 <270547+CodeBeast357@users.noreply.github.com>
2021-05-05 19:37:41 -0400

    Cleaned target framework netstandard1.3

--- src/Extensions.cs
+++ src/Extensions.cs
@@ -6,8 +6,6 @@ namespace JotunShard.Configuration
 {
     public static class Extensions
     {
-#if NETSTANDARD1_3
-
         public static IConfigurationBuilder AddFullPipeline<TServerConfigurationSource, TFileConfigurationSource>(
             this IConfigurationBuilder configurationBuilder,//AppDomain
             Action<TServerConfigurationSource> provideServerSource,
@@ -57,59 +55,5 @@ namespace JotunShard.Configuration
             configureSource?.Invoke(source);
             return builder.Add(source);
         }
-
-#endif
-
-#if NETSTANDARD2_0
-
-        public static IConfigurationBuilder AddFullPipeline<TServerConfigurationSource, TFileConfigurationSource>(
-            this IConfigurationBuilder configurationBuilder,
-            Action<TServerConfigurationSource> provideServerSource,
-            Action<TFileConfigurationSource> provideFileSource)
-            where TServerConfigurationSource : ServerConfigurationSource, new()
-            where TFileConfigurationSource : FileConfigurationSource, new()
-            => configurationBuilder
-                .AddEnvironmentVariables()
-                .Add(provideServerSource)
-                .Add(provideFileSource)
-                .Add<CommandLineConfigurationSource>(cmdSource =>
-                {
-                    cmdSource.Args = Environment.GetCommandLineArgs();
-                });
-
-        public static IConfigurationBuilder AddNetworkPipeline<TServerConfigurationSource, TFileConfigurationSource>(
-            this IConfigurationBuilder configurationBuilder,
-            Action<TServerConfigurationSource> provideServerSource)
-            where TServerConfigurationSource : ServerConfigurationSource, new()
-            => configurationBuilder
-                .AddEnvironmentVariables()
-                .Add(provideServerSource)
-                .Add<CommandLineConfigurationSource>(cmdSource =>
-                {
-                    cmdSource.Args = Environment.GetCommandLineArgs();
-                });
-
-        public static IConfigurationBuilder AddAgentPipeline<TServerConfigurationSource, TFileConfigurationSource>(
-            this IConfigurationBuilder configurationBuilder,
-            Action<TFileConfigurationSource> provideFileSource)
-            where TFileConfigurationSource : FileConfigurationSource, new()
-            => configurationBuilder
-                .AddEnvironmentVariables()
-                .Add(provideFileSource)
-                .Add<CommandLineConfigurationSource>(cmdSource =>
-                {
-                    cmdSource.Args = Environment.GetCommandLineArgs();
-                });
-
-        public static IConfigurationBuilder AddUtilityPipeline<TServerConfigurationSource, TFileConfigurationSource>(
-            this IConfigurationBuilder configurationBuilder)
-            => configurationBuilder
-                .AddEnvironmentVariables()
-                .Add<CommandLineConfigurationSource>(cmdSource =>
-                {
-                    cmdSource.Args = Environment.GetCommandLineArgs();
-                });
-
-#endif
     }
 }
