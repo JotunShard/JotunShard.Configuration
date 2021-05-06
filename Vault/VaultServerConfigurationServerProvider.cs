260c31c4a968333cebe22e90e9c677bd7f2e2bdd
CodeBeast357 <270547+CodeBeast357@users.noreply.github.com>
2021-05-05 20:03:27 -0400

    Added Vault Configuration (#2)

---
+++ Vault/VaultServerConfigurationServerProvider.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace JotunShard.Configuration.Vault
+{
+    public class VaultServerConfigurationServerProvider : IConfigurationServerProvider
+    {
+        public IConfigurationServer Create(IConfiguration connectionConfiguration)
+            => new VaultServerConfigurationServer(connectionConfiguration);
+    }
+}
