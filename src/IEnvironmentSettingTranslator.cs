194414cbd485f7a366afe4288e8fe4e9d6c536aa
CodeBeast357 <270547+CodeBeast357@users.noreply.github.com>
2021-08-10 15:37:38 -0400

    Updated design of extendable environment variables configuration

--- src/IEnvironmentSettingTranslator.cs
+++ src/IEnvironmentSettingTranslator.cs
@@ -5,12 +5,16 @@ namespace JotunShard.Configuration
 {
     public interface IEnvironmentSettingTranslator
     {
+        bool AcceptsKey(string key);
+
         IEnumerable<DictionaryEntry> Translate(DictionaryEntry entry);
     }
 
     public sealed class DefaultEnvironmentSettingTranslator : IEnvironmentSettingTranslator
     {
         public static IEnvironmentSettingTranslator Default { get; } = new DefaultEnvironmentSettingTranslator();
+
+        public bool AcceptsKey(string key) => true;
 
         public IEnumerable<DictionaryEntry> Translate(DictionaryEntry entry)
         {
